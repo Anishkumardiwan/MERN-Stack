2a7fbcc2f9a27ea9f701914287ee9a7aa14a2147
Diwan111 <69912582+Diwan111@users.noreply.github.com>
2022-01-30 13:54:27 +0530

    LoginPage

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,17 +1,45 @@
-import logo from './logo.svg';
+
+import { React, Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Component/Header/Header';
 import Footer from './Component/Footer/Footer';
 import Main from './Component/Main/Main';
+import LoginForm from './Component/LoginForm/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Main />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// Class Based Components ----------------------
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { isLoggedIn: false };
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          (this.state.isLoggedIn) ?
+            <>
+              <Header />
+              <Main />
+              <Footer />
+            </> :
+            <LoginForm />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
